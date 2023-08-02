e8d2fcf0e0c1aa6d6429985c50e32898496337c6
Atıf <dev.atifsimsek@gmail.com>
2023-08-02 11:11:53 +0300

    project created

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-blue-500 underline">
+        Hello world!
+      </h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;

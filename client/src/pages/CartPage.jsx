55d27c95b8a5eb61146e3981574ba1c918a2a95e
Atıf Şimşek <dev.atifsimsek@gmail.com>
2023-09-14 11:48:07 +0300

    create CartPage

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,11 +1,72 @@
 import React from "react";
 import Header from "../components/Header";
+import { Table, Card, Button } from "antd";
 
 const CartPage = () => {
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className="px-6">
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          bordered
+          pagination={false}
+        />
+        <div className=" flex justify-end mt-4">
+          <Card className="w-72 ">
+            <div className="flex justify-between">
+              <span>Ara Toplam</span>
+              <span>549.00₺</span>
+            </div>
+            <div className="flex justify-between my-2">
+              <span>Kdv Toplam %8</span>
+              <span className="text-red-600">549.00₺</span>
+            </div>
+            <div className="flex justify-between">
+              <b>Toplam</b>
+              <b>549.00₺</b>
+            </div>
+            <Button className="mt-2 w-full" type="primary" size="large">
+              Sipariş Oluştur
+            </Button>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 };
 

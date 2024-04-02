be39670a3c6291b00933c12434bcdb1b052de375
Atıf Şimşek <dev.atifsimsek@gmail.com>
2024-04-02 17:46:46 +0300

    update

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,6 +1,21 @@
 const Category = require("../models/Category");
 const express = require("express");
 const router = express.Router();
+
+// Get
+
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.send(categories);
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Post
+
 router.post("/add-category", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
@@ -8,6 +23,28 @@ router.post("/add-category", async (req, res) => {
     res.status(200).json("item added succesfully");
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+// Update
+
+router.put("/update-category/:id", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
+    res.status(200).json("item updated succesfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Delete
+
+router.delete("/delete-category/:id", async (req, res) => {
+  try {
+    await Category.findOneAndDelete({ _id: req.params.categoryId });
+    res.status(200).json("item deleted succesfully");
+  } catch (err) {
+    console.log(err);
   }
 });
 

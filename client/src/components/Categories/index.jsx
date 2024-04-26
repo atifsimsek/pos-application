866ddf9790b5c6307c19ce943009bd7289b61eee
Atıf <dev.atifsimsek@gmail.com>
2024-04-26 18:03:40 +0300

    add categories completed

--- client/src/components/Categories/index.jsx
+++ client/src/components/Categories/index.jsx
@@ -1,4 +1,46 @@
+import "./style.css";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Modal, message } from "antd";
+import { useState } from "react";
+
 const Categories = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "content-type": "application/json" },
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.status === 200) {
+            message.success("Kategori Başarıyla Eklendi");
+          } else {
+            message.error("Kategori Eklenirken Bir Hata Oluştu");
+          }
+        })
+        .finally(() => {
+          form.resetFields();
+          setIsModalOpen(false);
+        });
+    } catch (error) {
+      console.log(error);
+      setIsModalOpen(false);
+    }
+  };
+
   const categories = [
     { title: "Tümü" },
     { title: "Yiyecek" },
@@ -13,17 +55,48 @@ const Categories = () => {
   ];
 
   return (
-    <ul className="flex md:flex-col  gap-4 ">
-      {categories.map((category, index) => (
-        <li
-          key={index}
-          className="bg-green-500 px-6 py-10 text-white cursor-pointer
-      hover:bg-pink-600 transition-all text-center text-lg  min-w-[145px] "
-        >
-          <span>{category.title}</span>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="flex md:flex-col  gap-4 ">
+        {categories.map((category, index) =>
+          index === categories.length - 1 ? (
+            <li
+              className="category-item !bg-purple-800 hover:opacity-90"
+              onClick={showModal}
+            >
+              <PlusOutlined />
+            </li>
+          ) : (
+            <li key={index} className="category-item">
+              <span>{category.title}</span>
+            </li>
+          )
+        )}
+      </ul>
+      <Modal
+        title="Yeni Kategori Ekle"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item
+            name={"title"}
+            label="Kategori Ekle"
+            rules={[
+              { required: true, message: "Categori Alanı Boş Geçilemez" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Oluştur
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
 

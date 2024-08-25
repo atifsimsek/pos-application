5262a027fab0ddd054d38bcfeb9f1e9e8fd67836
Atıf <dev.atifsimsek@gmail.com>
2024-08-25 07:22:41 +0300

    Update apiClient.js

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
